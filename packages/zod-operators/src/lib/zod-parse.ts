e2807327e41fdeb0a3be99689b9d5aa74972b2ad
lgarcia <lucas.garcia@equasens.com>
2023-05-05 12:13:00 +0200

    feat (zod-operators):  improve the console.group with a default group name

--- packages/zod-operators/src/lib/zod-parse.ts
+++ packages/zod-operators/src/lib/zod-parse.ts
@@ -1,17 +1,21 @@
 import { MonoTypeOperatorFunction, Observable } from 'rxjs';
 import { SafeParseSuccess, ZodSchema } from 'zod';
 
-export const zodParse =
-  <T>(schema: ZodSchema<T>, options: { strict: boolean } = { strict: false }): MonoTypeOperatorFunction<T> =>
-  (source$: Observable<T>) =>
+let zodParseCount = 0;
+
+export const zodParse = <T>(
+  schema: ZodSchema<T>,
+  options: { strict: boolean } = { strict: false }
+): MonoTypeOperatorFunction<T> => {
+  const zodParseIndex = ++zodParseCount;
+
+  return (source$: Observable<T>) =>
     new Observable<T>((subscriber) =>
       source$.subscribe({
         next: (value) => {
           const parsedValue = schema.safeParse(value);
 
-          if (schema.description) {
-            console.group(schema.description);
-          }
+          console.group(schema.description ?? `zodParse#${zodParseIndex}`);
 
           if (parsedValue.success) {
             subscriber.next((parsedValue as SafeParseSuccess<T>).data);
@@ -30,3 +34,4 @@ export const zodParse =
         complete: () => subscriber.complete(),
       })
     );
+};
