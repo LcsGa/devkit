d11174a0327bf9124079b2551f1025dbaa272d4f
LcsGa <lcs.grc@outlook.com>
2023-05-09 07:22:40 +0200

    [#1] fix (operators): Add a take(1) on the duration selector of the `debounceMap` to avoid unexpected behaviors + fix a README typo

--- packages/operators/src/lib/debounce-map.ts
+++ packages/operators/src/lib/debounce-map.ts
@@ -1,8 +1,13 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { ObservableInput, ObservedValueOf, OperatorFunction, from, switchMap } from 'rxjs';
+import { ObservableInput, ObservedValueOf, OperatorFunction, from, switchMap, take } from 'rxjs';
 
 export const debounceMap = <TValue, TObservableInput extends ObservableInput<any>>(
   project: (value: TValue, index: number) => TObservableInput,
   durationSelector: (value: TValue) => ObservableInput<any>
 ): OperatorFunction<TValue, ObservedValueOf<TObservableInput>> =>
-  switchMap((value, index) => from(durationSelector(value)).pipe(switchMap(() => project(value, index))));
+  switchMap((value, index) =>
+    from(durationSelector(value)).pipe(
+      take(1),
+      switchMap(() => project(value, index))
+    )
+  );
