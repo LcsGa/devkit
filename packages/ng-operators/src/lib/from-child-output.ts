e07f5a414273c8158df67a3e2b4f054f52fa1000
lgarcia <lucas.garcia@equasens.com>
2023-05-17 14:06:09 +0200

    fix (ng-utils): infer the right return type

--- packages/ng-operators/src/lib/from-child-output.ts
+++ packages/ng-operators/src/lib/from-child-output.ts
@@ -3,17 +3,21 @@ import { inject, NgZone } from '@angular/core';
 import { ObservableInput, Observable, from, observeOn, asyncScheduler, switchMap, EMPTY } from 'rxjs';
 import { PickOutput } from './utils/pick-output.type';
 
-export const fromChildOutput = <TChild extends object, TOutput extends PickOutput<TChild>>(
+export const fromChildOutput = <
+  TChild extends object,
+  TOutput extends PickOutput<TChild>,
+  TOutputName extends keyof TOutput
+>(
   childSelector: () => TChild | undefined,
-  outputName: keyof TOutput,
+  outputName: TOutputName,
   { buildNotifier }: { buildNotifier?: ObservableInput<any> } = {}
-): Observable<TOutput[keyof TOutput]> => {
+): Observable<TOutput[TOutputName]> => {
   const ngZone = !buildNotifier ? inject(NgZone) : undefined;
 
   return (buildNotifier ? from(buildNotifier).pipe(observeOn(asyncScheduler)) : ngZone?.onMicrotaskEmpty ?? EMPTY).pipe(
     switchMap(() => {
       const child = childSelector() as unknown as TOutput;
-      return child ? (child[outputName] as Observable<TOutput[keyof TOutput]>) : EMPTY;
+      return child ? (child[outputName] as Observable<TOutput[TOutputName]>) : EMPTY;
     })
   );
 };
